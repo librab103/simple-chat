fce2597594bb1222613cfb8ec3f39fb59a4b12b4
Matt Battle <librab103@gmail.com>
2026-01-27 15:36:05 -0500

    Added emitting events and broadcasting

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('A user connected')
-})
+    console.log('A user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
 
 server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
